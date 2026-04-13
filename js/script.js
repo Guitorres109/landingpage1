44914980e63406618fb256ab6655b94cdadc4117
Guilherme <torres.guib2099@gmail.com>
2026-04-13 09:09:14 -0300

    Mudança na fonte

--- js/script.js
+++ js/script.js
@@ -16,5 +16,23 @@ function reveal() {
     items.forEach(item => observer.observe(item));
 }
 
+function mudarPalavras(){
+    const frases = [
+        "Profissional de Alta qualidade",
+        "Especialista em estética",
+        "Atendimento humanizado",
+        "Resultados incríveis",
+        "Confiança e excelência"
+    ];
+
+    let index = 0;
+    const elemento = document.getElementById("complemento");
+
+    setInterval(() => {
+        index = (index + 1) % frases.length;
+        elemento.textContent = frases[index];
+    }, 5000);
+}
+
 // Garante que o HTML já carregou
 document.addEventListener("DOMContentLoaded", reveal);
